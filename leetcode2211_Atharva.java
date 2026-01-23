e597cdd68041acd0c7d5f3cb513968612ab17cfc
Atharva MAlik <atharvasanjaymalik@gmail.com>
2026-01-23 23:01:23 +0530

    Implement countCollisions method in Solution class

---
+++ leetcode2211_Atharva.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countCollisions(String directions) {
+        int n = directions.length();
+        int l = 0, r = n - 1;
+
+    
+        while (l < n && directions.charAt(l) == 'L') {
+            l++;
+        }
+
+        
+        while (r >= 0 && directions.charAt(r) == 'R') {
+            r--;
+        }
+
+        int collisions = 0;
+
+        for (int i = l; i <= r; i++) {
+            if (directions.charAt(i) != 'S') {
+                collisions++;
+            }
+        }
+
+        return collisions;
+    }
+}
