36ae0656df135bf7adbde1c84727993339d85e42
Atharva MAlik <atharvasanjaymalik@gmail.com>
2026-05-26 11:11:35 +0530

    Implement deckRevealedIncreasing method

---
+++ LC950.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        int n=deck.length;
+        Arrays.sort(deck);
+        Deque<Integer> q=new ArrayDeque<>();
+        q.addFirst(deck[n-1]);
+
+        for(int i=n-2;i>=0;i--){
+            q.addFirst(q.removeLast());
+            q.addFirst(deck[i]);
+        }
+
+        for(int i=0;i<n;i++){
+            deck[i]=q.removeFirst();
+        }
+
+        return deck;
+    }
+}
