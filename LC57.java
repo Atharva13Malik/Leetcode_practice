ba513fdbafbc01c9f3252099dc0328b3daf1ab83
Atharva MAlik <atharvasanjaymalik@gmail.com>
2026-06-24 17:26:06 +0530

    Implement insert function for interval merging

---
+++ LC57.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+
+        List<int[]> result = new ArrayList<>();
+
+        int i = 0;
+        int n = intervals.length;
+
+        // Phase 1
+        while(i < n &&
+              intervals[i][1] < newInterval[0]) {
+
+            result.add(intervals[i]);
+            i++;
+        }
+
+        // Phase 2
+        while(i < n &&
+              intervals[i][0] <= newInterval[1]) {
+
+            newInterval[0] =
+                Math.min(newInterval[0],
+                         intervals[i][0]);
+
+            newInterval[1] =
+                Math.max(newInterval[1],
+                         intervals[i][1]);
+
+            i++;
+        }
+
+        // Phase 3
+        result.add(newInterval);
+
+        // Phase 4
+        while(i < n) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        return result.toArray(new int[0][]);
+    }
+}
