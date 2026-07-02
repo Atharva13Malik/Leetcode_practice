91aa2e729d5a5c66fa9320fcebaed60e0cf95fca
Atharva MAlik <atharvasanjaymalik@gmail.com>
2026-07-02 23:11:18 +0530

    Create segregate0and1.java

---
+++ segregate0and1.java
@@ -0,0 +1,15 @@
+class Solution {
+    void segregate0and1(int[] arr) {
+
+       int i=0;
+       int j=arr.length-1;
+       while(i<j){
+           if(arr[i]==0) i++;
+           if(arr[j]==1) j--;
+           else if(arr[i]==1 && arr[j]==0){
+             arr[i]=0;
+             arr[j]=1;
+           }
+       }
+    }
+}
