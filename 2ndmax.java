a6a764f19847bd1f35c982d5187f7f063cb5c13f
Atharva MAlik <atharvasanjaymalik@gmail.com>
2026-07-02 20:47:45 +0530

    Implement finding the maximum value in an array
    Fix array initialization and return statement in main method.

---
+++ 2ndmax.java
@@ -0,0 +1,11 @@
+public class Main{
+public static void main(String args[]){
+
+  int arr={4,5,6,7,10,10};
+  int max=Integer.MIN_VALUE;
+  for(int i=0;i<arr.length;i++){
+    if(arr[i}>max) max=arr[i];
+    
+  }
+  return max;
+}
