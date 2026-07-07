798e3d9cfbc64529d8c6c3c1400bc6587be74195
Atharva Malik <atharvasanjaymalik@gmail.com>
2026-07-07 11:40:57 +0530

    Implement minimumDeleteSum method in Solution class

---
+++ LC_712.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int minimumDeleteSum(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        // First column
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = dp[i - 1][0] + s.charAt(i - 1);
+        }
+
+        // First row
+        for (int j = 1; j <= m; j++) {
+            dp[0][j] = dp[0][j - 1] + t.charAt(j - 1);
+        }
+
+        // Fill DP table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(
+                            dp[i - 1][j] + s.charAt(i - 1),
+                            dp[i][j - 1] + t.charAt(j - 1)
+                    );
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+}
