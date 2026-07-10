f46fede1a9c02dd8736eb6f0d859797a113db2ae
Atharva Malik <atharvasanjaymalik@gmail.com>
2026-07-10 14:36:53 +0530

    Add getIntersectionNode method in Solution class
    Implement method to find intersection of two linked lists.

---
+++ LC_160.java
@@ -0,0 +1,41 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode temp1=headA;
+        ListNode temp2=headB;
+        int len1=0;
+        int len2=0;
+
+        while(temp1!=null){
+            len1++;
+            temp1=temp1.next;
+
+        }
+
+        while(temp2!=null){
+            len2++;
+            temp2=temp2.next;
+        }
+
+        temp1=headA;
+        temp2=headB;
+
+        if(len1>len2){
+            for(int i=0;i< len1-len2 ;i++){
+                temp1=temp1.next;
+            }
+        }
+
+         if(len2>len1){
+            for(int i=0;i< len2-len1 ;i++){
+                temp2=temp2.next;
+            }
+        }
+
+        while(temp1!=temp2){
+            temp1=temp1.next;
+            temp2=temp2.next;
+        }
+
+        return temp1;
+    }
+}
