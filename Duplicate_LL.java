44d5a8f6fd3c3aa91b7f686a5bcca8757391bdb1
Atharva Malik <atharvasanjaymalik@gmail.com>
2026-07-10 15:41:53 +0530

    Add deleteDuplicates method to remove duplicates
    Implement a method to delete duplicates from a sorted linked list.

---
+++ Duplicate_LL.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null) return null;
+        ListNode i=head;
+        ListNode j=head;
+
+        while(j!=null ){
+            if(i.val!=j.val){
+                i.next=j;
+                i=j;
+            }
+            j=j.next;
+        }
+        i.next=j;
+        return head;
+    }
+}
