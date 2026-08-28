8151034b5ecd4baf8b290578cb95b0f4a7513095
Atharva Malik <atharvasanjaymalik@gmail.com>
2026-08-28 18:46:55 +0530

    Add minSwap method for minimum swaps calculation
    Implement minSwap method to calculate minimum swaps needed to bring elements less than or equal to k together.

---
+++ Minimum Swaps Required to Bring Elements ≤ K.java
@@ -0,0 +1,28 @@
+class Solution {
+    int minSwap(int[] arr, int k) {
+     
+        // Code Here
+        int good=0;
+        int bad=0;
+        for(int x:arr){
+            if(x<=k) good++;
+        }
+        
+        for(int i=0;i<good;i++){
+            if(arr[i]>k) bad++;
+        }
+        
+        int left=0;
+        int right=good;
+        int ans=bad;
+        
+        while(right<arr.length){
+            if(arr[right]>k) bad++;
+            if(arr[left]>k) bad--;
+            ans=Math.min(ans,bad);
+            left++;
+            right++;
+        }
+        return ans;
+    }
+}
