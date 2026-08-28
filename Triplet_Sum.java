57364e87493f0f0c627d2dce8db1f8fd79ce5b20
Atharva Malik <atharvasanjaymalik@gmail.com>
2026-08-28 13:43:55 +0530

    Add triplet sum checking method
    Implement a method to check for triplet sum in an array.

---
+++ Triplet_Sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean hasTripletSum(int arr[], int target) {
+        // code Here
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            int left=i+1;
+            int right=arr.length-1;
+            
+            while(left<right){
+                int sum=arr[i]+arr[left]+arr[right];
+                
+                if(sum==target){
+                    return true;
+                }
+                else if(sum>target){
+                    right--;
+                }
+                else left++;
+            }
+        }
+        return false;
+        
+    }
+}
