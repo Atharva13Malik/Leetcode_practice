1d08b897579d6a32466a246219bb91641f63e9c0
Atharva Malik <atharvasanjaymalik@gmail.com>
2026-08-29 10:31:50 +0530

    Implement binary search in 2D matrix

---
+++ Search-In-2dMatrix.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int start=0;
+        int end=m*n-1;
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            int row=mid/n;
+            int col=mid%n;
+            int val=matrix[row][col];
+
+            if(val==target){
+                return true;
+            }
+            else if(val>target){
+                end=mid-1;
+            }
+            else start=mid+1;
+            
+        }
+        return false;
+    }
+
+}
