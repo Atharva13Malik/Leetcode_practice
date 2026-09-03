c87abb2259736fa39e0c8910cd0d816733780150
Atharva Malik <atharvasanjaymalik@gmail.com>
2026-09-03 11:17:18 +0530

    Implement repeatedSubstringPattern method

---
+++ LC_459.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+
+        String doubled = s + s;
+
+        String temp = doubled.substring(1, doubled.length() - 1);
+
+        return temp.contains(s);
+    }
+}
